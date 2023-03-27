e7e7cf6f897e3b1512572c79aea2421ddd0889de
turtle5996 <125853038+turtle5996@users.noreply.github.com>
2023-03-27 09:00:07 +0900

    Add files via upload

---
+++ 예제3-7.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+class Circle {
+	
+public:
+	int radius;
+	double getArea();
+	Circle();
+	Circle(int r);
+	~Circle();
+};
+Circle::Circle() {
+	radius = 1;
+	cout << "반지름" << radius << "원 생성" << endl;
+}
+Circle::Circle(int r) {
+	radius = r;
+	cout << "반지름" << radius << "원 생성" << endl;
+}
+Circle::~Circle() {
+	cout << "반지름" << radius << "원 소멸" << endl;
+}
+double Circle::getArea() {
+	return 3.14 * radius * radius;
+}
+int main() {
+	Circle wa;
+	Circle pi(30);
+	return 0;
+}
---
+++ 예제3-9 (오류 예시).cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+class  PrivateAccessError {
+private:
+	int a;
+	void f();
+	PrivateAccessError();
+public:
+	int b;
+	PrivateAccessError(int x);
+	void g();
+};
+
+PrivateAccessError::PrivateAccessError() {
+	a = 1;
+	b = 1;
+}
+
+PrivateAccessError::PrivateAccessError(int x) {
+	a = x;
+	b = x;
+}
+void PrivateAccessError::f() {
+	a = 5;
+	b = 5;
+}
+void PrivateAccessError::g() {
+	a = 6;
+	b = 6;
+}
+
+int main() {
+	PrivateAccessError objA;//private로 오류
+	PrivateAccessError objB(2800);
+	objB.a = 10;//a가 private로 오류
+	objB.b = 20;
+	objB.f();//f()가 private로 오류
+	objB.g();
+
+}
